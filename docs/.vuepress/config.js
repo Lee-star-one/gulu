8a9ba7fe0797a1d88d894dda281b9bfd5f32b682
eLee-star-one <lixingxing@Longshine.com>
2020-12-21 18:17:34 +0800

    提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   base:'/gulu/',
   title:'eco-UI',
+  dest: './dist',
   description:'一个好用的UI框架',
+  repo: 'https://github.com/Lee-star-one/gulu',
   themeConfig: {
     nav: [
       { text: '主页', link: '/' },
